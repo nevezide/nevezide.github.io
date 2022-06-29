a9e5a5f99287092897b0678a913b3b05808beef0
nevezide <nevezide44@gmail.com>
2022-06-29 22:34:33 +0200

    feat(JS exos) Improve design, add tags and levels

--- Javascript/demo/index.js
+++ Javascript/demo/index.js
@@ -1,2 +1,29 @@
+// toto++ => toto = toto + 1;
+for (let toto = 0; toto < 3; toto++) {
+  console.log(toto);
+}
 
-console.log("Hello World");
+// Premiere iteration
+let toto = 0;
+
+if (toto < 3) {
+  console.log(toto);
+}
+
+// Seconde iteration
+toto = toto + 1
+if (toto < 3) {
+  console.log(toto)
+}
+
+// Troisieme iteration
+toto = toto + 1
+if (toto < 3) {
+  console.log(toto)
+}
+
+// Derniere iteration
+toto = toto + 1
+if (toto < 3) {
+  //console.log(toto)
+}
