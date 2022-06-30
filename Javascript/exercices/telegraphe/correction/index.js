aeb42f2c52217c868e525abfcd68c3b54bde83a7
nevezide <nevezide44@gmail.com>
2022-06-30 15:19:44 +0200

    fix(JS exos) Replace var by let

--- Javascript/exercices/telegraphe/correction/index.js
+++ Javascript/exercices/telegraphe/correction/index.js
@@ -1,9 +1,9 @@
-var alphabet = [
+let alphabet = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
-var codeMorse = [
+let codeMorse = [
   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
   ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
   "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
@@ -12,16 +12,16 @@ var codeMorse = [
 ];
 
 // Demander à l'utilisateur de saisir un texte à télégraphier
-var userInput = prompt("Saisir le texte à télégraphier");
+let userInput = prompt("Saisir le texte à télégraphier");
 
 // Convertir le texte en code morse
-var userInputArray = userInput.split("");
-var userInputArrayLength = userInputArray.length;
-var outputCodeMorse = [];
+let userInputArray = userInput.split("");
+let userInputArrayLength = userInputArray.length;
+let outputCodeMorse = [];
 
-for (var i = 0; i < userInputArrayLength; i++) {
-  var userInputArrayElement = userInputArray[i];
-  var position = alphabet.indexOf(userInputArrayElement);
+for (let i = 0; i < userInputArrayLength; i++) {
+  let userInputArrayElement = userInputArray[i];
+  let position = alphabet.indexOf(userInputArrayElement);
 
   outputCodeMorse.push(
     codeMorse[position]
