930582674d58fdc4c54e2358ca6ad3b3db2e834d
nevezide <nevezide44@gmail.com>
2022-06-30 14:52:05 +0200

    fix(JS exo) add missing word list on word-counter

--- Javascript/exercices/word-counter/exercice/index.js
+++ Javascript/exercices/word-counter/exercice/index.js
@@ -1,5 +1,7 @@
 /** Compte le nombre de mots commençant par s */
 
+let liste_mots = ['sapin', 'bougie', 'sucre', 'soleil', 'alphabet', 'trousse'];
+
 // Parcourir la liste de mots
 
   // Vérifie si la première lettre est égale à s
