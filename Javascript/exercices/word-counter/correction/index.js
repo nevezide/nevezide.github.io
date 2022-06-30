83acd302c608a37caf738cf495fed3f0e2215450
nevezide <nevezide44@gmail.com>
2022-06-30 13:22:49 +0200

    refacto(JS exos) Refacto word counter

---
+++ Javascript/exercices/word-counter/correction/index.js
@@ -0,0 +1,32 @@
+/** Compte le nombre de mots commençant par s */
+
+let liste_mots = ['sapin', 'bougie', 'sucre', 'soleil', 'alphabet', 'trousse'];
+
+// Parcourir la liste de mots
+let compteur = 0;
+for(let i = 0; i < liste_mots.length; i++) {
+  let mot = liste_mots[i];
+
+  // Vérifie si la première lettre est égale à s
+  if(mot[0] === 's') {
+    // Si oui, incrémente le compteur
+    compteur++;
+  }
+}
+
+// Affiche le nombre de mots commençant par s
+console.log(`${compteur} lettres commençant par s`);
+
+// Seconde méthode
+/*
+let liste_mots = ['sapin', 'bougie', 'sucre', 'soleil', 'alphabet', 'trousse'];
+let compteur = 0;
+
+liste_mots.forEach((mot) => {
+  if(mot[0] === 's') {
+    compteur++;
+  }
+});
+
+console.log(`${compteur} lettres commençant par s`);
+*/
---
+++ Javascript/exercices/word-counter/exercice/index.js
@@ -0,0 +1,8 @@
+/** Compte le nombre de mots commençant par s */
+
+// Parcourir la liste de mots
+
+  // Vérifie si la première lettre est égale à s
+    // Si oui, incrémente le compteur
+
+// Affiche le nombre de mots commençant par s
