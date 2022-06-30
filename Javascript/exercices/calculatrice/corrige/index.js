fa6e6d81f29ffc8151936976ad9f4a638acaa7be
nevezide <nevezide44@gmail.com>
2022-06-30 11:49:48 +0200

    refacto(JS exos) Refacto calculatrice exo

---
+++ Javascript/exercices/calculatrice/corrige/index.js
@@ -0,0 +1,33 @@
+
+// Demander à l'utilisateur d'entrer un premier nombre
+let userInput1 = parseFloat(
+  prompt("Entrez un nombre")
+);
+// Demander à l'utilisateur d'entrer un opérateur
+let userOperator = prompt("Entrez un opérateur");
+
+// Demander à l'utilisateur d'entrer un second nombre
+let userInput2 = parseFloat(
+  prompt("Entrez un second nombre")
+);
+
+// Vérifier que l'utilisateur a bien entré un nombre
+// (lui afficher un message si c'est pas le cas)
+if(isNaN(userInput1) || isNaN(userInput2)) {
+  alert("Vous n'avez pas entré un nombre");
+}
+// Vérifier que l'utilisateur a bien entré un opérateur valide (+ - * /)
+// (lui afficher un message si c'est pas le cas)
+// Effectuer l'opération demandée en fonction de l'opérateur
+// Afficher le résultat de l'opération à l'utilisateur
+else if (userOperator === "+") {
+  alert(userInput1 + userInput2);
+} else if (userOperator === "-") {
+  alert(userInput1 - userInput2);
+} else if (userOperator === "*") {
+  alert(userInput1 * userInput2);
+} else if (userOperator === "/") {
+  alert(userInput1 / userInput2);
+} else {
+  alert("Vous n'avez pas entré un opérateur autorisé");
+}
