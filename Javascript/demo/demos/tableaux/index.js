aa170b7839b2e8cd045e5c493c2cfe11a524cf39
nevezide <nevezide44@gmail.com>
2022-07-01 17:27:13 +0200

    doc(JS demo) Add array vulgarised concept

--- Javascript/demo/demos/tableaux/index.js
+++ Javascript/demo/demos/tableaux/index.js
@@ -1,3 +1,14 @@
+/*
+
+ Un tableau est comme une armoire avec des tiroirs.
+ - Dans chaque tiroir, il y a un élément (string, number, booléen, tableau, ...)
+ - Chaque tiroir est identifié par une étiquette (appelée index).
+ Le premier tiroir est identifié par l'index 0
+ Le second par l'index 1, ...
+
+ On accède au contenu du tiroir par son étiquette
+
+*/
 
 let toto = ["banane", "pomme", "poire"];
 
